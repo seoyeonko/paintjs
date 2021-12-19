8a1e0a39b887a9e5de187154d835615702958310
Seoyeon Ko <55572222+seoyeonko@users.noreply.github.com>
2021-12-20 01:16:27 +0900

    Brush size

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d'); // 캔버스에 그림그리는 도구를 얻어내기
 const colors = document.getElementsByClassName('jsColor');
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
 
 // canvas size
 canvas.width = 700;
@@ -10,6 +12,7 @@ ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -40,6 +43,22 @@ function handleColorClick(event) {
   ctx.strokeStyle = color; // 색 변경
 }
 
+function handleRangeChange(event) {
+  const size = event.target.value;
+  console.log(event.target.value);
+  ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = 'Fill';
+  } else {
+    filling = true;
+    mode.innerText = 'Paint';
+  }
+}
+
 if (canvas) {
   // mousemove: 마우스 움직였을 때
   canvas.addEventListener('mousemove', onMouseMove);
@@ -56,7 +75,14 @@ if (canvas) {
 }
 
 // Array.from: object로 부터 array를 만듦
-console.log(Array.from(colors));
 Array.from(colors).forEach((color) =>
   color.addEventListener('click', handleColorClick)
 );
+
+if (range) {
+  range.addEventListener('input', handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener('click', handleModeClick);
+}
